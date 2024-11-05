d5882be0df8058c6098c9090f8c196279d078ba2
ASHISH KUMAR DUBEY <ashishdubeyak04@gmail.com>
2024-11-06 00:32:33 +0530

    blog features

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
 const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
 const { checkForAuthCookie } = require("./middlewares/auth");
+const Blog = require("./models/blog");
 
 const app = express(); 
 const PORT = 8000;
@@ -19,14 +22,18 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
 app.use(checkForAuthCookie("token"));
+app.use(express.static(path.resolve('./public')))
 
-app.get('/', (req,res)=>{
+app.get('/', async(req,res)=>{
+    const allBlogs = await Blog.find({});
     res.render("home", {
         user: req.user,
+        blogs: allBlogs,
     });
 })
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
 
 app.listen(PORT, () =>{
